76a46c4cd788d13b9d5b92e5eeefde177998b5b2
UJ09 <42847901+UJ09@users.noreply.github.com>
2018-11-19 00:04:34 +0530

    Add files via upload

---
+++ Blind_stick.ino
@@ -0,0 +1,50 @@
+#define trigPin 13
+#define echoPin 12
+#define buzzer 6
+void setup()
+{
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(buzzer,OUTPUT);
+}
+void loop()
+{
+long duration, distance;
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(2); 
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10); 
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration*0.034)/2.0;
+if ((distance >75)&&(distance<100))
+{ 
+digitalWrite(buzzer,HIGH);
+delay(100);
+digitalWrite(buzzer,LOW);
+}
+else if((distance>50)&&(distance<75))
+{
+  digitalWrite(buzzer,HIGH);
+  delay(400);
+  digitalWrite(buzzer,LOW);
+}
+else if((distance>25)&&(distance<50))
+{
+  digitalWrite(buzzer,HIGH);
+  delay(700);
+  digitalWrite(buzzer,LOW);
+}
+else if(distance<25)
+{
+  digitalWrite(buzzer,HIGH);
+  delay(1000);
+  digitalWrite(buzzer,LOW);
+}
+else
+{
+digitalWrite(buzzer,LOW); 
+}
+digitalWrite(buzzer,LOW);
+delay(200);
+}
